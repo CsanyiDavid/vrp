06f43430a7be162ae42589bedb44d553687c0238
Csanyi David <csanyidavid2000@gmail.com>
2020-12-22 09:53:02 +0100

    Create main function in main.cc

--- main.cc
+++ main.cc
@@ -1,4 +1,16 @@
 //
 // Created by david on 2020. 12. 22..
 //
+#include <iostream>
+#include <lemon/list_graph.h>
 
+using namespace std;
+using namespace lemon;
+
+int main(){
+    cout << "Hello" << endl;
+    ListDigraph g;
+    g.addNode();
+    cout << countNodes(g) << endl;
+    return 0;
+}
